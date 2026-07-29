976bef6701b93e75b360af63af482d8bf1524810
Kenneth LEE <edowon0623@gmail.com>
2026-07-30 08:28:31 +0900

    kafka connect  실습 예제 추가

--- user-service/src/main/java/com/example/userservice/Utils.java
+++
@@ -1,9 +0,0 @@
-package com.example.userservice;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class Utils {
-    public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("password"));
-    }
-}
